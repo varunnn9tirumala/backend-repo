7e05e59b0931e429bbc5a5d26dc3eb0ed2919011
varunnn9tirumala <varuntirumala570@gmail.com>
2026-03-02 11:54:39 +0530

    fix cors for vercel frontend

--- server.js
+++ server.js
@@ -8,7 +8,17 @@ dotenv.config()
 
 const app = express()
 
-app.use(cors())
+app.use(
+cors({
+origin:[
+"https://tripcraft-frontend.vercel.app",
+"http://localhost:5173"
+],
+methods:["GET","POST"],
+credentials:true
+})
+)
+
 app.use(express.json())
 
 app.use("/api", saraAI)
